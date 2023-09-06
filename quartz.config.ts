da1f876afe7d558f2ab426e88144206fa96ffd1b
quantumgardener <david@buchan.org>
2023-09-06 19:35:32 +1000

    Quartz sync: Sep 6, 2023, 7:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -4,7 +4,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "The Quantum Garden",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
