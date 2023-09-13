7bea4505e88bf11abf7e972001b737a885db6269
quantumgardener <david@buchan.org>
2023-09-13 21:32:54 +1000

    Quartz sync: Sep 13, 2023, 9:32 PM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,8 +3,14 @@ import { QuartzComponentConstructor } from "../types"
 function NotFound() {
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>Either this page is private or doesn't exist.</p>
+      <h1>Page Not Found</h1>
+      <p>Sadly, the page you are looking for can't be found. There can be several reasons for this.</p>
+      <ul>
+        <li>The page doesn't exist and never has.</li>
+        <li>Content has not yet migrated across from an earlier version of the website.</li>
+        <li>Something was typed incorrectly.</li>
+      </ul>
+      <p>Please return to the <a href="/">Home page</a> where you can use Search to find what you are seeking.</p>
     </article>
   )
 }
