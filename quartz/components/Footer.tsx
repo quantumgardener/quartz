69d9efe08228962372abfe8df97361e7007cc5df
quantumgardener <david@buchan.org>
2023-09-16 14:50:54 +1000

    Automatically generate fa-icon from text

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,14 +17,13 @@ export default ((opts?: Options) => {
           © David C. Buchan 2002&ndash;{year}. Created with <a href="https://quartz.jzhao.xyz/">Quartz</a>.
         </p>
         <ul>
-          {/* {Object.entries(links).map(([text, link]) => (
+          {Object.entries(links).map(([text, link]) => {
+            const fontclass = "fa-brands fa-" + text.toLowerCase()
+            return (
             <li>
-              <a href={link}>{text}</a>
+              <a href={link}><i class={fontclass}></i> {text}</a>
             </li>
-          ))} */}
-          <li><a href="https://aus.social/@dcbuchan"><i class="fa-brands fa-mastodon"></i> Mastodon</a></li>
-          <li><a href="https://www.flickr.com/photos/dcbuchan/"><i class="fa-brands fa-flickr"></i> Flickr</a></li>
-          <li><a href="https://www.linkedin.com/in/buchan/"><i class="fa-brands fa-linkedin"></i> LinkedIn</a></li>
+          )})}
         </ul>
       </footer>
     )
