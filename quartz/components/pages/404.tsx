cd9fffd4d2774bb12ccc9304c55a45757039aa6d
quantumgardener <david@buchan.org>
2023-09-20 21:40:47 +1000

    Embed 404 as any other page

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,14 +3,13 @@ import { QuartzComponentConstructor } from "../types"
 function NotFound() {
   return (
     <article class="popover-hint">
-      <h1>Page Not Found</h1>
       <p>Sadly, the page you are looking for can't be found. There can be several reasons for this.</p>
       <ul>
         <li>The page doesn't exist and never has.</li>
         <li>Content has not yet migrated across from an earlier version of the website.</li>
         <li>Something was typed incorrectly.</li>
       </ul>
-      <p>Please return to the <a href="/">Home page</a> where you can use Search to find what you are seeking.</p>
+      <p>Please return to the <a href="/">Home page</a> or use the <strong>Search box</strong> to find what you are seeking.</p>
     </article>
   )
 }
