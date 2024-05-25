72eb3a16ba1f698e2fb7143bfc078dbd6ee6a086
quantumgardener <david@buchan.org>
2024-05-25 17:57:58 +1000

    Set visual titles to proper TitleCase
    Add a titlecase function to article titles for the proper display of title cases, not just an uppercase the first letter, and lowercase the rest.

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,13 +1,47 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
+// from https://stackoverflow.com/posts/13897813/revisions
+declare global {
+    interface String {
+      toTitleCase(): string;
+    }
+}
+
+// from https://stackoverflow.com/posts/6475125/revisions
+String.prototype.toTitleCase = function() {
+  var i, j, str, lowers, uppers;
+  str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+
+  // Certain minor words should be left lowercase unless 
+  // they are the first or last words in the string
+  lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
+  'By', 'For', 'From', 'In', 'Into', 'Is', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+  for (i = 0, j = lowers.length; i < j; i++)
+    str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+      function(txt) {
+        return txt.toLowerCase();
+      });
+
+  // Certain words such as initialisms or acronyms should be left uppercase
+  uppers = ['Id', 'Tv', 'Moc'];
+  for (i = 0, j = uppers.length; i < j; i++)
+    str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+      uppers[i].toUpperCase());
+
+  return str;
+}
+
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
   if (title) {
-    if (title.startsWith("Topic: ")) {
-      return <h1 class="article-title"><i class="fa-regular fa-message"></i>&nbsp;&nbsp;{title.replace("Topic: ","")}</h1>
+    const workingTitle = title.toTitleCase()
+    if (workingTitle.startsWith("Topic: ")) {
+      return <h1 class="article-title"><i class="fa-regular fa-message"></i>&nbsp;&nbsp;{workingTitle.replace("Topic: ","")}</h1>
     } else {
-      return <h1 class={`article-title ${displayClass ?? ""}`}>{title}</h1>
+      return <h1 class={`article-title ${displayClass ?? ""}`}>{workingTitle}</h1>
     }
   } else {
     return null
