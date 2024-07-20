635b0370a321fc56a6e25b3fdf4803583ef934e9
quantumgardener <david@buchan.org>
2024-07-20 18:06:54 +1000

    Update quartz.config.ts
    Change language to en-GB

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,7 @@ const config: QuartzConfig = {
     enableSPA: false,
     enablePopovers: true,
     analytics: null,
-    locale: "en-US",
+    locale: "en-GB",
     baseUrl: "quantumgardener.info",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
