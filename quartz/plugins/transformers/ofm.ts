5ee0c8b203c5df4a5d3b8bc04b445bd442f22961
quantumgardener <david@buchan.org>
2024-08-07 21:10:50 +1000

    Dataview Serialiser improvements
    Handle .webp so that thumbnails can be displayed in tables.

--- quartz/plugins/transformers/ofm.ts
+++ quartz/plugins/transformers/ofm.ts
@@ -243,9 +243,16 @@ export const ObsidianFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> 
           // Check for links coming in from Obsidian Dataview Serializer 
           // https://github.com/dsebastien/obsidian-dataview-serializer?tab=readme-ov-file
           // These have full path and .md extension
-          if (fp.slice(-3) == ".md") {
-            // We have a Dataview Serializer Path. Strip it down to filename without path or extension
-            fp = path.basename(fp).substring(0, path.basename(fp).lastIndexOf('.'))
+          //if (fp.endsWith(".md") || (fp.startsWith("Quartz"))  ) {
+          //if (fp.endsWith(".md")) {
+          if (fp.startsWith("Quartz")) {
+              // We have a Dataview Serializer Path. Strip it down to filename without path or extension
+              if (fp.endsWith(".md")) {
+                fp = path.basename(fp).substring(0, path.basename(fp).lastIndexOf('.'))
+              }
+              if (fp.endsWith(".webp")) {
+                fp = path.basename(fp)
+              }
           }
 
           // Only link pages that actually exist in the graph
